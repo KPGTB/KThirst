0b55a34a05f3a4865ec0f39f2e86a415094aeacd
KPG-TB <kpg2k6@gmail.com>
2022-06-05 15:24:03 +0200

    init

---
+++ src/main/java/io/github/kpgtb/kkthirst/KKthirst.java
@@ -0,0 +1,17 @@
+package io.github.kpgtb.kkthirst;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KKthirst extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
